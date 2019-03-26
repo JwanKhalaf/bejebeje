aff10ff29a4da82988f298a8fc47647f3910d5b7
J Khalaf <jwan.khalaf@outlook.com>
2019-03-26 16:12:28 +0000

    added the artist controller

---
+++ Bejebeje.Api/Controllers/ArtistController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bejebeje.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArtistController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
