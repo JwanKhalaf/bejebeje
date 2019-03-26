849966505b1fac61320d614731228fe872f2bd6c
J Khalaf <jwan.khalaf@outlook.com>
2019-03-26 16:26:08 +0000

    added a project to house the domain classes, and added an artist class

---
+++ Bejebeje.Domain/Artist.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bejebeje.Domain
+{
+    public class Artist
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Slug { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
