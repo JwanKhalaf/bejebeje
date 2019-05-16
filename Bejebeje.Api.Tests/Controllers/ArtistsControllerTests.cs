546db2e2a29200d8187eb1169841cb947bc7d894
J Khalaf <jwan.khalaf@outlook.com>
2019-05-16 13:43:06 +0100

    added a test class for the artists controller

---
+++ Bejebeje.Api.Tests/Controllers/ArtistsControllerTests.cs
@@ -0,0 +1,28 @@
+﻿namespace Bejebeje.Api.Tests.Controllers
+{
+  using Bejebeje.Api.Controllers;
+  using Bejebeje.Services.Services.Interfaces;
+  using Moq;
+  using NUnit.Framework;
+
+  [TestFixture]
+  public class ArtistsControllerTests
+  {
+    private Mock<IArtistsService> artistsServiceMock;
+
+    private ArtistsController artistsController;
+
+    [SetUp]
+    public void Setup()
+    {
+      artistsServiceMock = new Mock<IArtistsService>(MockBehavior.Strict);
+      artistsController = new ArtistsController(artistsServiceMock.Object);
+    }
+
+    [Test]
+    public void EnsureThatAnEmptyListIsReturnedWhenThereIsNoData()
+    {
+
+    }
+  }
+}
