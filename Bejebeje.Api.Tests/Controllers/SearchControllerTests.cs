65af889bf04c03797ddffc2ae5ffd1a8b77eaa0e
Jwan Khalaf <jwan.khalaf@outlook.com>
2019-05-25 14:06:21 +0100

    added a failing unit test (the red in red green refactor) to ensure the search controller throws an argument null exception when the argument is null

---
+++ Bejebeje.Api.Tests/Controllers/SearchControllerTests.cs
@@ -0,0 +1,33 @@
+﻿namespace Bejebeje.Api.Tests.Controllers
+{
+  using System;
+  using System.Threading.Tasks;
+  using Bejebeje.Api.Controllers;
+  using FluentAssertions;
+  using NUnit.Framework;
+
+  [TestFixture]
+  public class SearchControllerTests
+  {
+    private SearchController searchController;
+
+    [SetUp]
+    public void Setup()
+    {
+      searchController = new SearchController();
+    }
+
+    [Test]
+    public async Task Search_WhenParamIsNull_ThrowsArgumentNullException()
+    {
+      // arrange
+      string searchTerm = null;
+
+      // act
+      Func<Task> action = async () => await searchController.Search(searchTerm);
+
+      // assert
+      await action.Should().ThrowAsync<ArgumentNullException>();
+    }
+  }
+}
---
+++ Bejebeje.Api/Controllers/SearchController.cs
@@ -0,0 +1,14 @@
+﻿namespace Bejebeje.Api.Controllers
+{
+  using System;
+  using System.Threading.Tasks;
+  using Microsoft.AspNetCore.Mvc;
+
+  public class SearchController : ControllerBase
+  {
+    public Task Search(string searchTerm)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
