46e251b74cfde138fda585668a44aa243d9fd1fa
Jwan Khalaf <jwan.khalaf@outlook.com>
2019-05-26 13:58:14 +0100

    added a unit test to ensure that when there is data, the ok object result has the correct and expected data

--- Bejebeje.Api.Tests/Controllers/SearchControllerTests.cs
+++ Bejebeje.Api.Tests/Controllers/SearchControllerTests.cs
@@ -2,22 +2,37 @@
 {
   using System;
   using System.Collections.Generic;
+  using System.Linq;
   using System.Threading.Tasks;
   using Bejebeje.Api.Controllers;
+  using Bejebeje.Common.Enums;
+  using Bejebeje.Services.Services.Interfaces;
   using Bejebeje.ViewModels.Search;
   using FluentAssertions;
   using Microsoft.AspNetCore.Mvc;
+  using Microsoft.Extensions.Logging;
+  using Moq;
   using NUnit.Framework;
 
   [TestFixture]
   public class SearchControllerTests
   {
+    private Mock<ISearchService> searchServiceMock;
+
+    private Mock<ILogger<SearchController>> loggerMock;
+
     private SearchController searchController;
 
     [SetUp]
     public void Setup()
     {
-      searchController = new SearchController();
+      searchServiceMock = new Mock<ISearchService>(MockBehavior.Strict);
+
+      loggerMock = new Mock<ILogger<SearchController>>(MockBehavior.Loose);
+
+      searchController = new SearchController(
+        searchServiceMock.Object,
+        loggerMock.Object);
     }
 
     [Test]
@@ -52,11 +67,58 @@
       // arrange
       string searchTerm = "james brown";
 
+      searchServiceMock
+        .Setup(x => x.SearchAsync(searchTerm))
+        .ReturnsAsync(new List<SearchResultViewModel>());
+
       // act
       IActionResult result = await searchController.Search(searchTerm);
 
       // assert
       result.Should().BeOfType<OkObjectResult>();
+    }
+
+    [Test]
+    public async Task Search_WhenResultsExist_ReturnsAPopulatedListOfSearchResultViewModel()
+    {
+      // arrange
+      string searchTerm = "fats waller";
+      string expectedSlugInResult = "fats-waller";
+      string expectedNameInResult = "Fats Waller";
+      int expectedImageIdInResult = 1;
+      ResultType expectedResultType = ResultType.Artist;
+
+      searchServiceMock
+        .Setup(x => x.SearchAsync(searchTerm))
+        .ReturnsAsync(new List<SearchResultViewModel>
+        {
+          new SearchResultViewModel
+          {
+            Slug = expectedSlugInResult,
+            Name = expectedNameInResult,
+            ImageId = expectedImageIdInResult,
+            ResultType = expectedResultType
+          }
+        });
+
+      // act
+      IActionResult result = await searchController.Search(searchTerm);
+
+      // assert
+      result.Should().BeOfType<OkObjectResult>();
+
+      OkObjectResult okObjectResult = result as OkObjectResult;
+
+      okObjectResult.Should().NotBeNull();
+
+      List<SearchResultViewModel> searchResults = okObjectResult.Value as List<SearchResultViewModel>;
+
+      searchResults.Should().NotBeNull();
+      searchResults.Should().HaveCount(1);
+      searchResults.First().Slug.Should().Be(expectedSlugInResult);
+      searchResults.First().Name.Should().Be(expectedNameInResult);
+      searchResults.First().ImageId.Should().Be(expectedImageIdInResult);
+      searchResults.First().ResultType.Should().Be(expectedResultType);
     }
   }
 }
--- Bejebeje.Api/Controllers/SearchController.cs
+++ Bejebeje.Api/Controllers/SearchController.cs
@@ -1,19 +1,37 @@
 ﻿namespace Bejebeje.Api.Controllers
 {
   using System;
+  using System.Collections.Generic;
   using System.Threading.Tasks;
+  using Bejebeje.Services.Services.Interfaces;
+  using Bejebeje.ViewModels.Search;
   using Microsoft.AspNetCore.Mvc;
+  using Microsoft.Extensions.Logging;
 
   public class SearchController : ControllerBase
   {
-    public Task<IActionResult> Search(string searchTerm)
+    private ISearchService searchService;
+
+    private ILogger logger;
+
+    public SearchController(
+      ISearchService searchService,
+      ILogger<SearchController> logger)
+    {
+      this.searchService = searchService;
+      this.logger = logger;
+    }
+
+    public async Task<IActionResult> Search(string searchTerm)
     {
       if (string.IsNullOrEmpty(searchTerm))
       {
         throw new ArgumentNullException(nameof(searchTerm));
       }
 
-      throw new NotImplementedException();
+      IList<SearchResultViewModel> searchResults = await searchService.SearchAsync(searchTerm);
+
+      return Ok(searchResults);
     }
   }
 }
