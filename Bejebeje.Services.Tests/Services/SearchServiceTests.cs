6370bc23df777c91195293934266717a78dcb5b9
Jwan Khalaf <jwan.khalaf@outlook.com>
2019-05-26 14:53:10 +0100

    added search service test class and a test to make sure an empty list is returned when no results match search term

---
+++ Bejebeje.Services.Tests/Services/SearchServiceTests.cs
@@ -0,0 +1,46 @@
+﻿namespace Bejebeje.Services.Tests.Services
+{
+  using System.Collections;
+  using System.Collections.Generic;
+  using System.Threading.Tasks;
+  using Bejebeje.Services.Services;
+  using Bejebeje.Services.Tests.Helpers;
+  using Bejebeje.ViewModels.Search;
+  using FluentAssertions;
+  using Microsoft.Extensions.Logging;
+  using Moq;
+  using NUnit.Framework;
+
+  [TestFixture]
+  public class SearchServiceTests : DatabaseTestBase
+  {
+    private Mock<ILogger<SearchService>> loggerMock;
+
+    private SearchService searchService;
+
+    [SetUp]
+    public void Setup()
+    {
+      loggerMock = new Mock<ILogger<SearchService>>(MockBehavior.Loose);
+
+      SetupDataContext();
+
+      searchService = new SearchService(loggerMock.Object);
+    }
+
+
+    [Test]
+    public async Task SearchAsync_WhenNoResultsMatch_ReturnsAnEmptyListOfSearchResultViewModels()
+    {
+      // arrange
+      string searchTerm = "queen";
+
+      // act
+      IList<SearchResultViewModel> results = await searchService.SearchAsync(searchTerm);
+
+      // assert
+      results.Should().NotBeNull();
+      results.Should().HaveCount(0);
+    }
+  }
+}
---
+++ Bejebeje.Services/Services/SearchService.cs
@@ -0,0 +1,23 @@
+﻿namespace Bejebeje.Services.Services
+{
+  using System.Collections.Generic;
+  using System.Threading.Tasks;
+  using Bejebeje.Services.Services.Interfaces;
+  using Bejebeje.ViewModels.Search;
+  using Microsoft.Extensions.Logging;
+
+  public class SearchService : ISearchService
+  {
+    private ILogger logger;
+
+    public SearchService(ILogger<SearchService> logger)
+    {
+      this.logger = logger;
+    }
+
+    public Task<IList<SearchResultViewModel>> SearchAsync(string searchTerm)
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+}
