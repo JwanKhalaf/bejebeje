93afa26423f18a60baef9e07e7b6544e105f18bb
Jwan Khalaf <jwan.khalaf@outlook.com>
2020-01-20 11:20:52 +0000

    git rid of leading whitespace on literal strings

--- Bejebeje.Services/Services/DataSeederService.cs
+++ Bejebeje.Services/Services/DataSeederService.cs
@@ -33,9 +33,9 @@
           FullName = "AC/DC",
           Biography = @"AC/DC are an Australian rock band formed in Sydney in 1973 by Scottish-born brothers Malcolm and Angus Young. Their music has been variously described as hard rock, blues rock, and heavy metal; however, the band themselves describe their music as simply 'rock and roll'.
           
-          AC/DC underwent several line-up changes before releasing their first album, High Voltage, in 1975. Membership subsequently stabilised around the Young brothers, singer Bon Scott, drummer Phil Rudd, and bass player Mark Evans. Evans was replaced by Cliff Williams in 1977 for the album Powerage.
+AC/DC underwent several line-up changes before releasing their first album, High Voltage, in 1975. Membership subsequently stabilised around the Young brothers, singer Bon Scott, drummer Phil Rudd, and bass player Mark Evans. Evans was replaced by Cliff Williams in 1977 for the album Powerage.
           
-          In February 1980, a few months after recording the album Highway to Hell, lead singer and co-songwriter Bon Scott died of acute alcohol poisoning. The group considered disbanding but stayed together, bringing in Brian Johnson as replacement for Scott. Later that year, the band released their first album with Johnson, Back in Black, which they dedicated to Scott's memory. The album launched them to new heights of success and became one of the best selling albums of all time.",
+In February 1980, a few months after recording the album Highway to Hell, lead singer and co-songwriter Bon Scott died of acute alcohol poisoning. The group considered disbanding but stayed together, bringing in Brian Johnson as replacement for Scott. Later that year, the band released their first album with Johnson, Back in Black, which they dedicated to Scott's memory. The album launched them to new heights of success and became one of the best selling albums of all time.",
           Slugs = new List<AuthorSlug>
               {
                 new AuthorSlug
@@ -56,9 +56,9 @@
           FullName = "Chester Arthur Burnett",
           Biography = @"Chester Arthur Burnett (June 10, 1910 – January 10, 1976), known as Howlin' Wolf, was a Chicago blues singer, guitarist, and harmonica player, originally from Mississippi.
           
-          With a booming voice and imposing physical presence, he is one of the best-known Chicago blues artists. The musician and critic Cub Koda noted, 'no one could match Howlin' Wolf for the singular ability to rock the house down to the foundation while simultaneously scaring its patrons out of its wits.'
+With a booming voice and imposing physical presence, he is one of the best-known Chicago blues artists. The musician and critic Cub Koda noted, 'no one could match Howlin' Wolf for the singular ability to rock the house down to the foundation while simultaneously scaring its patrons out of its wits.'
           
-          Producer Sam Phillips recalled, 'When I heard Howlin' Wolf, I said, 'This is for me. This is where the soul of man never dies.' Several of his songs, including 'Smokestack Lightnin', 'Killing Floor' and 'Spoonful', have become blues and blues rock standards. In 2011, Rolling Stone magazine ranked him number 54 on its list of the '100 Greatest Artists of All Time'.",
+Producer Sam Phillips recalled, 'When I heard Howlin' Wolf, I said, 'This is for me. This is where the soul of man never dies.' Several of his songs, including 'Smokestack Lightnin', 'Killing Floor' and 'Spoonful', have become blues and blues rock standards. In 2011, Rolling Stone magazine ranked him number 54 on its list of the '100 Greatest Artists of All Time'.",
           Slugs = new List<AuthorSlug>
               {
                 new AuthorSlug
@@ -81,36 +81,36 @@
         {
           Title = "TNT",
           MarkdownBody = @"Oi, oi, oi
-          Oi, oi, oi
-          Oi, oi, oi
-          Oi, oi, oi
-          Oi, oi, oi
-          
-          See me ride out of the sunset
-          On your color TV screen
-          Out for all that I can get
-          If you know what I mean
-          Women to the left of me
-          And women to the right
-          Ain't got no gun
-          Ain't got no knife
-          Don't you start no fight
-          
-          'Cause I'm T.N.T. I'm dynamite
-          T.N.T. and I'll win the fight
-          T.N.T. I'm a power load
-          T.N.T. watch me explode
-          
-          I'm dirty, mean and mighty unclean
-          I'm a wanted man
-          Public enemy number one
-          Understand
-          So lock up your daughter
-          Lock up your wife
-          Lock up your back door
-          And run for your life
-          The man is back in town
-          Don't you mess me 'round",
+Oi, oi, oi
+Oi, oi, oi
+Oi, oi, oi
+Oi, oi, oi
+
+See me ride out of the sunset
+On your color TV screen
+Out for all that I can get
+If you know what I mean
+Women to the left of me
+And women to the right
+Ain't got no gun
+Ain't got no knife
+Don't you start no fight
+
+'Cause I'm T.N.T. I'm dynamite
+T.N.T. and I'll win the fight
+T.N.T. I'm a power load
+T.N.T. watch me explode
+
+I'm dirty, mean and mighty unclean
+I'm a wanted man
+Public enemy number one
+Understand
+So lock up your daughter
+Lock up your wife
+Lock up your back door
+And run for your life
+The man is back in town
+Don't you mess me 'round",
           UserId = userId,
           Slugs = new List<LyricSlug>
               {
@@ -130,33 +130,33 @@
         {
           Title = "Thunderstruck",
           MarkdownBody = @"I was caught
-          In the middle of a railroad track (thunder)
-          I looked round
-          And I knew there was no turning back (thunder)
-          My mind raced
-          And I thought what could I do (thunder)
-          And I knew
-          There was no help, no help from you (thunder)
-          Sound of the drums
-          Beating in my heart
-          The thunder of guns
-          Tore me apart
-          You've been
-          Thunderstruck
-          
-          Rode down the highway
-          Broke the limit, we hit the town
-          Went through to Texas, yeah Texas, and we had some fun
-          We met some girls
-          Some dancers who gave a good time
-          Broke all the rules
-          Played all the fools
-          Yeah yeah they, they, they blew our minds
-          And I was shaking at the knees
-          Could I come again please
-          Yeah them ladies were too kind
-          You've been
-          Thunderstruck",
+In the middle of a railroad track (thunder)
+I looked round
+And I knew there was no turning back (thunder)
+My mind raced
+And I thought what could I do (thunder)
+And I knew
+There was no help, no help from you (thunder)
+Sound of the drums
+Beating in my heart
+The thunder of guns
+Tore me apart
+You've been
+Thunderstruck
+
+Rode down the highway
+Broke the limit, we hit the town
+Went through to Texas, yeah Texas, and we had some fun
+We met some girls
+Some dancers who gave a good time
+Broke all the rules
+Played all the fools
+Yeah yeah they, they, they blew our minds
+And I was shaking at the knees
+Could I come again please
+Yeah them ladies were too kind
+You've been
+Thunderstruck",
           UserId = userId,
           Slugs = new List<LyricSlug>
               {
@@ -176,32 +176,32 @@
         {
           Title = "The Thrill Is Gone",
           MarkdownBody = @"The thrill is gone
-          The thrill is gone away
-          The thrill is gone baby
-          The thrill is gone away
-          You know you done me wrong baby
-          And you'll be sorry someday
-          
-          The thrill is gone
-          It's gone away from me
-          The thrill is gone baby
-          The thrill is gone away from me
-          Although, I'll still live on
-          But so lonely I'll be
-          
-          The thrill is gone
-          It's gone away for good
-          The thrill is gone baby
-          It's gone away for good
-          Someday I know I'll be over it all baby
-          Just like I know a good man should
-          
-          You know I'm free, free now baby
-          I'm free from your spell
-          Oh I'm free, free, free now
-          I'm free from your spell
-          And now that it's all over
-          All I can do is wish you well",
+The thrill is gone away
+The thrill is gone baby
+The thrill is gone away
+You know you done me wrong baby
+And you'll be sorry someday
+
+The thrill is gone
+It's gone away from me
+The thrill is gone baby
+The thrill is gone away from me
+Although, I'll still live on
+But so lonely I'll be
+
+The thrill is gone
+It's gone away for good
+The thrill is gone baby
+It's gone away for good
+Someday I know I'll be over it all baby
+Just like I know a good man should
+
+You know I'm free, free now baby
+I'm free from your spell
+Oh I'm free, free, free now
+I'm free from your spell
+And now that it's all over
+All I can do is wish you well",
           UserId = userId,
           Slugs = new List<LyricSlug>
               {
