7c37367979d78b703aa960d5646bfea47a181884
Jwan Khalaf <jwan.khalaf@outlook.com>
2020-02-09 13:49:33 +0000

    add echo statements to see what branch of the if statement is activated

--- scripts/docker-publish-travis.sh
+++ scripts/docker-publish-travis.sh
@@ -17,9 +17,11 @@ echo "The travis branch variable is:"
 echo $TRAVIS_BRANCH
 
 if [ $TRAVIS_BRANCH == "develop" ]; then
+  echo "we're in true branch!"
   # tag the docker image with latest develop tag.
   docker tag bejebeje/api:$DOCKER_TAG $DOCKER_USERNAME/bejebeje/api:$DOCKER_DEVELOP_TAG
 else
+  echo "we're in false branch!"
   # tag the docker image with latest.
   docker tag bejebeje/api:$DOCKER_TAG $DOCKER_USERNAME/bejebeje/api:$DOCKER_TAG
 fi
