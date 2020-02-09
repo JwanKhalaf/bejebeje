587e9a2e9b2d26005c8a4a33bc86e131845c6ff5
Jwan Khalaf <jwan.khalaf@outlook.com>
2020-02-09 14:16:26 +0000

    removed echo statements

--- scripts/docker-publish-travis.sh
+++ scripts/docker-publish-travis.sh
@@ -1,35 +1,30 @@
 #!/bin/bash
 
 DOCKER_TAG='latest'
-DOCKER_DEVELOP_TAG='develop'
+DOCKER_DEVELOP_TAG='latest-develop'
 
 # log into docker hub.
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
 # build the docker image.
-docker build -f ./Dockerfile -t bejebeje/api:$DOCKER_TAG -t bejebeje/api:$TRAVIS_BUILD_NUMBER . --no-cache
+docker build -f ./Dockerfile -t bejebeje/api:travis . --no-cache
 
 # tag the docker image with build number.
-docker tag bejebeje/api:$DOCKER_TAG $DOCKER_USERNAME/bejebeje/api:$TRAVIS_BUILD_NUMBER
-
-echo "The travis branch variable is:"
-
-echo $TRAVIS_BRANCH
+docker tag bejebeje/api:travis $DOCKER_USERNAME/bejebeje/api:$TRAVIS_BUILD_NUMBER
 
 if [ "$TRAVIS_BRANCH" == "develop" ]; then
-  echo "we're in true branch!"
-  echo "the develop tag is:"
-  echo $DOCKER_DEVELOP_TAG
   # tag the docker image with latest develop tag.
-  docker tag bejebeje/api:$DOCKER_TAG $DOCKER_USERNAME/bejebeje/api:$DOCKER_DEVELOP_TAG
-else
-  echo "we're in false branch!"
-  # tag the docker image with latest.
-  docker tag bejebeje/api:$DOCKER_TAG $DOCKER_USERNAME/bejebeje/api:$DOCKER_TAG
-fi
+  docker tag bejebeje/api:travis $DOCKER_USERNAME/bejebeje/api:$DOCKER_DEVELOP_TAG
 
-# push the docker image (tagged latest) to docker hub.
-docker push bejebeje/api:$DOCKER_TAG
+  # push the docker image (tagged latest-develop) to docker hub.
+  docker push bejebeje/api:$DOCKER_DEVELOP_TAG
+else
+  # tag the docker image with latest.
+  docker tag bejebeje/api:travis $DOCKER_USERNAME/bejebeje/api:$DOCKER_TAG
+  
+  # push the docker image (tagged latest) to docker hub.
+  docker push bejebeje/api:$DOCKER_TAG
+fi
 
 # push the docker image (tagged with build number) to docker hub.
 docker push bejebeje/api:$TRAVIS_BUILD_NUMBER
