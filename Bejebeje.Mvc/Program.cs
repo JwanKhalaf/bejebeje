c65eb9bf823a4206451e4b5a4be59d66935cba21
Jwan Khalaf <jwan.khalaf@outlook.com>
2020-08-26 20:46:46 +0100

    format startup.cs and program.cs files

--- Bejebeje.Mvc/Program.cs
+++ Bejebeje.Mvc/Program.cs
@@ -13,14 +13,17 @@ namespace Bejebeje.Mvc
   {
     public static void Main(string[] args)
     {
-      CreateHostBuilder(args).Build().Run();
+      CreateHostBuilder(args)
+        .Build()
+        .Run();
     }
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
             .ConfigureWebHostDefaults(webBuilder =>
             {
-              webBuilder.UseStartup<Startup>();
+              webBuilder
+                .UseStartup<Startup>();
             });
   }
 }
--- Bejebeje.Mvc/Startup.cs
+++ Bejebeje.Mvc/Startup.cs
@@ -20,37 +20,53 @@ namespace Bejebeje.Mvc
 
     public IConfiguration Configuration { get; }
 
-    // This method gets called by the runtime. Use this method to add services to the container.
+    // this method gets called by the runtime.
+    // use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddControllersWithViews();
+      services
+        .AddControllersWithViews();
     }
 
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    // this method gets called by the runtime.
+    // yse this method to configure the http request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
       if (env.IsDevelopment())
       {
-        app.UseDeveloperExceptionPage();
+        app
+          .UseDeveloperExceptionPage();
       }
       else
       {
-        app.UseExceptionHandler("/Home/Error");
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
+        app
+          .UseExceptionHandler("/Home/Error");
+
+        // the default hsts value is 30 days.
+        // you may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app
+          .UseHsts();
       }
-      app.UseHttpsRedirection();
-      app.UseStaticFiles();
 
-      app.UseRouting();
+      app
+        .UseHttpsRedirection();
 
-      app.UseAuthorization();
+      app
+        .UseStaticFiles();
 
-      app.UseEndpoints(endpoints =>
+      app
+        .UseRouting();
+
+      app
+        .UseAuthorization();
+
+      app
+        .UseEndpoints(endpoints =>
       {
-        endpoints.MapControllerRoute(
-                  name: "default",
-                  pattern: "{controller=Home}/{action=Index}/{id?}");
+        endpoints
+          .MapControllerRoute(
+            "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
       });
     }
   }
