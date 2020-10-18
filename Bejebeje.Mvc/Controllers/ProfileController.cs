5248b3d6b2bfbea10257f6e3a0a6784a52cc9868
Jwan Khalaf <jwan.khalaf@outlook.com>
2020-10-18 21:12:20 +0100

    implement new design

---
+++ Bejebeje.Mvc/Controllers/ProfileController.cs
@@ -0,0 +1,12 @@
+﻿namespace Bejebeje.Mvc.Controllers
+{
+  using Microsoft.AspNetCore.Mvc;
+
+  public class ProfileController : Controller
+  {
+    public IActionResult Index()
+    {
+      return View();
+    }
+  }
+}
