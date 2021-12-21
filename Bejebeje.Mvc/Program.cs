4811df31dad236a3b5e69282956a73e2e64f8341
Jwan Khalaf <jwan.khalaf@outlook.com>
2021-12-21 21:24:01 +0000

    make necessary changes for the .net 6 upgrade

--- Bejebeje.Mvc/Program.cs
+++ Bejebeje.Mvc/Program.cs
@@ -1,34 +1,111 @@
-namespace Bejebeje.Mvc
-{
-  using System;
-  using System.Net;
-  using Microsoft.AspNetCore.Hosting;
-  using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Bejebeje.Services.Services;
+using Bejebeje.Services.Services.Interfaces;
+using Bejebeje.DataAccess.Context;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Bejebeje.Services.Config;
 
-  public class Program
-  {
-    public static void Main(string[] args)
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+/* IdentityModelEventSource.ShowPII = true; */
+string authority = builder.Configuration["IdentityServerConfiguration:Authority"];
+System.Console.WriteLine("Authority is:");
+System.Console.WriteLine(authority);
+string clientId = builder.Configuration["IdentityServerConfiguration:ClientId"];
+System.Console.WriteLine("Client Id is:");
+System.Console.WriteLine(clientId);
+string clientSecret = builder.Configuration["IdentityServerConfiguration:ClientSecret"];
+string connectionString = builder.Configuration["ConnectionString"];
+
+builder.Services.Configure<DatabaseOptions>(builder.Configuration);
+
+builder.Services.AddDbContext<BbContext>(options => options
+      .UseNpgsql(connectionString)
+      .UseSnakeCaseNamingConvention());
+
+builder.Services.AddScoped<IImagesService, ImagesService>();
+
+builder.Services.AddScoped<IArtistSlugsService, ArtistSlugsService>();
+
+builder.Services.AddScoped<IArtistsService, ArtistsService>();
+
+builder.Services.AddScoped<ILyricsService, LyricsService>();
+
+builder.Services.AddScoped<ISitemapService, SitemapService>();
+
+builder.Services.AddAuthentication(options =>
     {
-      CreateHostBuilder(args)
-        .Build()
-        .Run();
-    }
+      options.DefaultScheme = "Cookies";
+      options.DefaultChallengeScheme = "oidc";
+    })
+    .AddCookie("Cookies")
+    .AddOpenIdConnect("oidc", options =>
+    {
+      options.Authority = authority;
+      options.RequireHttpsMetadata = false;
+      options.ClientId = clientId;
+      options.ClientSecret = clientSecret;
+      options.ResponseType = "code";
+      options.SaveTokens = true;
+      options.GetClaimsFromUserInfoEndpoint = true;
+      options.Scope.Clear();
+      options.Scope.Add("openid");
+      options.ClaimActions.MapUniqueJsonKey("role", "role");
+      options.TokenValidationParameters = new TokenValidationParameters { RoleClaimType = "role" };
+    });
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-              webBuilder
-                .UseStartup<Startup>()
-                .UseSentry(options =>
-                {
-                  options.Release = "1";
-                  options.MaxBreadcrumbs = 200;
-                  options.HttpProxy = null;
-                  options.DecompressionMethods = DecompressionMethods.None;
-                  options.MaxQueueItems = 100;
-                  options.ShutdownTimeout = TimeSpan.FromSeconds(5);
-                });
-            });
-  }
+builder.Services.AddControllersWithViews();
+
+JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+var app = builder.Build();
+
+ForwardedHeadersOptions forwardedHeadersOptions = new ForwardedHeadersOptions
+{
+  ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+};
+
+forwardedHeadersOptions.KnownNetworks.Clear();
+forwardedHeadersOptions.KnownProxies.Clear();
+
+app.UseForwardedHeaders(forwardedHeadersOptions);
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+  app.UseExceptionHandler("/Error");
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+  endpoints
+    .MapControllerRoute(
+      "default",
+  pattern: "{controller=Home}/{action=Index}/{id?}");
+});
+
+app.Run();
